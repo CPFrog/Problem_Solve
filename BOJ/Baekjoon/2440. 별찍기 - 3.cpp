483612e4fca7088b4f14187ee9da0e2fe33f882f
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-07 09:53:42 +0900

    Implementation

---
+++ BOJ/Baekjoon/2440. 별찍기 - 3.cpp
@@ -0,0 +1,15 @@
+// submitted at 2018.07.30 16:25:28
+#include <iostream>
+using namespace std;
+
+int main() {
+	int rep;
+	cin >> rep;
+
+	for (int i = 0; i < rep; i++) {
+		for (int j = rep; j - i > 0; j--)
+			cout << '*';
+		cout << endl;
+	}
+	return 0;
+}
