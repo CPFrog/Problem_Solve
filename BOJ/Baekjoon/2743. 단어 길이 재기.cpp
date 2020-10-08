e0652e0af51494c602dd2f43e626ee3506222b18
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-08 09:50:33 +0900

    String

---
+++ BOJ/Baekjoon/2743. 단어 길이 재기.cpp
@@ -0,0 +1,14 @@
+// submitted at 2018.07.30. 18:12:36
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string a;
+	cin > > a;
+
+	cout << a.length() << '\n';
+	return 0;
+}
