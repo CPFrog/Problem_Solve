ebf8209cb84829dab913f05b8e7b57ce9f3d94cc
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-08 09:46:14 +0900

    Implementation

---
+++ BOJ/Baekjoon/2742. 기찍 N.cpp
@@ -0,0 +1,14 @@
+// submitted at 2018.07.30. 18:04:59
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int a;
+	cin >> a;
+	for (int i = a; i > 0; i--)
+		cout << i << '\n';
+	return 0;
+}
