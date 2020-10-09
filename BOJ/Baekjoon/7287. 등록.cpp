57684f686670c7a501d51e8935816eda072d380d
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-09 18:27:43 +0900

    Implementation

---
+++ BOJ/Baekjoon/7287. 등록.cpp
@@ -0,0 +1,11 @@
+// submitted at 2018.07.30. 18:22:45
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "37" << '\n' << "kokk012 \n";
+	return 0;
+}
