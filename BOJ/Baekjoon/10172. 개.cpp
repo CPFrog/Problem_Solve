2ba2035c890a503333a8fdc33f6b2f09037418aa
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-09 19:05:24 +0900

    Implementation

---
+++ BOJ/Baekjoon/10172. 개.cpp
@@ -0,0 +1,9 @@
+// submitted at 2018.07.30. 18:52:24
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|\n" << "|q p|   " << "/}" << "\n" << "( 0 )\"\"\"\\" << "\n" << "|\"^\"`    |" << "\n"
+		<< "||_/=\\\\__|";
+	return 0;
+}
