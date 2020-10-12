b9d9724a717dc09214f4ae9fc5ee8a43b9b5bbd4
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-12 14:07:22 +0900

    Implementation

---
+++ BOJ/Baekjoon/10718. We love kriii.cpp
@@ -0,0 +1,11 @@
+// submitted at 18.07.30. 18:58:04
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	for (int i = 0; i < 2; i++)
+		cout << "강한친구 대한육군" << '\n';
+	return 0;
+}
