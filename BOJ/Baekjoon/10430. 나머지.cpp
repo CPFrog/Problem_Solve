0ab287709a43da1ad264742d51fd9a5bb9fde7f6
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-12 10:08:40 +0900

    Implementation

---
+++ BOJ/Baekjoon/10430. 나머지.cpp
@@ -0,0 +1,12 @@
+// submitted at 2018.07.30. 18:55:46
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c;
+	cout << (a + b) % c << '\n' << (a % c + b % c) % c << '\n' << (a * b) % c << '\n' << (a % c * b % c) % c << '\n';
+	return 0;
+}
