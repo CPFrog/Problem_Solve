2bbf0b676bb9516c65811884976b5633a2b0137a
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-10-13 15:39:02 +0900

    Stack

---
+++ BOJ/Baekjoon/10799 쇠막대기.cpp
@@ -0,0 +1,29 @@
+// submitted at 2018.08.05 22:03:18
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	stack<int> s;
+	string p;
+	cin >> p;
+	int pcs = 0;
+	for (int i = 0; i < p.length(); i++) {
+		if (p.at(i) == '(') s.push(i);
+		else {
+			if (s.top() + 1 == i) {
+				s.pop();
+				pcs += s.size();
+			}
+			else {
+				s.pop();
+				pcs += 1;
+			}
+		}
+	}
+	cout << pcs << '\n';
+	return 0;
+}
