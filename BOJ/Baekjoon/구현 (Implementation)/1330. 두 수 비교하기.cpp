811a52c6f0e6a59bff81f9a6661bf0789cb5ffb4
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-11-03 13:00:30 +0900

    2020.06.19. 13:24:10

---
+++ BOJ/Baekjoon/구현 (Implementation)/1330. 두 수 비교하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+
+	if (a > b) cout << ">" << '\n';
+	else if (a == b) cout << "==" << '\n';
+	else cout << "<" << '\n';
+	return 0;
+}
