6a4e9eee17899c5e83126ddccb236bc2735e3068
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-11-06 10:12:22 +0900

    2020.06.19. 16:38:57

---
+++ BOJ/Baekjoon/구현 (Implementation)/9654. 나부 함대 데이터.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << '\n' <<
+		6
+		"N2 Bomber      Heavy Fighter  Limited    21        " << '\n' <<
+		7
+		"J-Type 327     Light Combat   Unlimited  1         " << '\n' <<
+		8
+		"NX Cruiser     Medium Fighter Limited    18        " << '\n' <<
+		9
+		"N1 Starfighter Medium Fighter Unlimited  25        " << '\n' <<
+		10
+		"Royal Cruiser  Light Combat   Limited    4         " << '\n';
+	return 0;
+}
