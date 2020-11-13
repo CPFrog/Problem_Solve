681d44d06abe3e7236b5d29276e7640d893be6a8
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-11-13 17:31:56 +0900

    2020.06.21. 17:46:19

---
+++ BOJ/Baekjoon/구현 (Implementation)/10926. ？？！.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	string s;
+	cin >> s;
+	cout << s << "??!" << '\n';
+	return 0;
+}
