15b47dcace89625c39b38cafb1ea31ab2b8ea771
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-12-15 15:50:23 +0900

    2020.08.04. 16:06:17

---
+++ BOJ/Baekjoon/수학 (Math)/10973. 이전 순열.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+
+	int a[n];
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	if (prev_permutation(a, a + n)) {
+		for (int i = 0; i < n; i++)
+			cout << a[i] << ' ';
+		cout << '\n';
+	}
+	else cout << -1 << '\n';
+	return 0;
+}
