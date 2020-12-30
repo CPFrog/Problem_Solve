2217453a22fa73f29d66314781a0b5e62e2ba41f
CPFrog <35889115+CPFrog@users.noreply.github.com>
2020-12-30 09:18:03 +0900

    2020.10.08.16:49:29

---
+++ BOJ/Baekjoon/정렬 (Sort)/11004. K번째 수.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	cout << v[k - 1] << '\n';
+	return 0;
+}
