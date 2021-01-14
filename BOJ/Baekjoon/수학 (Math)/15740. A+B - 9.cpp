6b414a0b79f0533d42c282b766b23f5861884ad6
CPFrog <35889115+CPFrog@users.noreply.github.com>
2021-01-14 11:05:39 +0900

    2020.11.29. 19:36:29

---
+++ BOJ/Baekjoon/수학 (Math)/15740. A+B - 9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+//50Á¡
+
+int pick(string& s) {
+	if (s.empty()) return 0;
+	int p = (int)s.back() - '0';
+	s.pop_back();
+	return p;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	string a, b;
+	cin >> a >> b;
+
+	stack<int> s;
+
+	int ml = max(a.length(), b.length());
+	int temp = 0;
+	for (int i = 0; i < ml; i++) {
+		temp += pick(a) + pick(b);
+		if (temp > 9) {
+			s.push(temp - 10);
+			temp = 1;
+		}
+		else {
+			s.push(temp);
+			temp = 0;
+		}
+		if (i == ml - 1 && temp)
+			s.push(temp);
+	}
+	while (!s.empty()) {
+		cout << s.top();
+		s.pop();
+	}
+	cout << '\n';
+	return 0;
+}
