0f3e7109465140a99726f4e32ca5b55e603f810e
CPFrog <35889115+CPFrog@users.noreply.github.com>
2021-01-20 13:54:22 +0900

    2020.12.11. 13:44:36

---
+++ BOJ/Baekjoon/큐 (Queue)/2164. 카드2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+	whlie(q.size() != 1) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front() << '\n';
+	return 0;
+}
